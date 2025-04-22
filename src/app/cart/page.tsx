e6ef32a442cbbbfd0aa4c29763fc208c34be461d
pjg <jjo3335@gmail.com>
2025-04-22 19:51:34 +0900

    완

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -12,34 +12,41 @@ import StaticFooter from "@/components/footers/static-footer";
 import { useQuery } from "@tanstack/react-query";
 import { getCartList } from "@/lib/api/cart";
 import Loading from "@/components/loading";
+import { useAuthStore } from "@/store/auth.store";
 
 function Cart() {
+  const auth = useAuthStore((s) => s.auth);
   const cartList = useQuery({
     queryKey: ["getCartList"],
     queryFn: getCartList,
+    enabled: !!auth,
   });
 
-  return cartList.isPending ? (
-    <Loading />
-  ) : cartList.isError ? (
-    <div>error</div>
-  ) : (
+  return (
     <AuthGuard>
-      <CartHeader />
-      <CartWrap>
-        <ShippingAddress />
-        <StickyMenu />
-        {cartList.data.length === 0 ? (
-          <div className="flex h-50 items-center justify-center text-xs text-gray-500">장바구니가 비었습니다.</div>
-        ) : (
-          cartList.data.map((cartItem) => <CartItem key={cartItem.cartId} cartItem={cartItem} />)
-        )}
-        {cartList.data.length !== 0 && <Summaries cartList={cartList.data.filter(({ checked }) => checked)} />}
-        <Notice />
+      {cartList.isPending ? (
+        <Loading />
+      ) : cartList.isError ? (
+        <div>error</div>
+      ) : (
+        <>
+          <CartHeader />
+          <CartWrap>
+            <ShippingAddress />
+            <StickyMenu />
+            {cartList.data.length === 0 ? (
+              <div className="flex h-50 items-center justify-center text-xs text-gray-500">장바구니가 비었습니다.</div>
+            ) : (
+              cartList.data.map((cartItem) => <CartItem key={cartItem.cartId} cartItem={cartItem} />)
+            )}
+            {cartList.data.length !== 0 && <Summaries cartList={cartList.data.filter(({ checked }) => checked)} />}
+            <Notice />
 
-        <CartFooter cartList={cartList.data.filter(({ checked }) => checked)} />
-      </CartWrap>
-      <StaticFooter />
+            <CartFooter cartList={cartList.data.filter(({ checked }) => checked)} />
+          </CartWrap>
+          <StaticFooter />
+        </>
+      )}
     </AuthGuard>
   );
 }
