0dae232e63cfb377376302a7f43b03afba99f013
bababu1 <zx99242@gmail.com>
2025-04-22 11:00:15 +0900

    보더, 호버 삭제

--- src/app/all-products/_ui/product-card.tsx
+++ src/app/all-products/_ui/product-card.tsx
@@ -9,7 +9,7 @@ export default function ProductCard({ product }: Props) {
   console.log("📦 productId:", product.productId); // ✅ 여기에 위치해야 정상 작동
 
   return (
-    <Link href={`/product/${product.productId}`} className="relative z-50 block border hover:shadow">
+    <Link href={`/product/${product.productId}`} className="relative z-50 block">
       <div className="cursor-pointer">
         <img src={product.imageThumbUrl} alt={product.name} className="w-full pb-3" />
         <div className="pb-3 text-[15px] font-semibold">{product.name}</div>
